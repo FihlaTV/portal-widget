d660db26dbc25e6e7fff1246bbea1425dc9f8063
Bharat Ramaswamy Nandakumar <bnandaku@hawk.iit.edu>
2017-01-05 13:52:09 -0600

    Added a line to handle ringback (#119)

--- public/javascripts/controllers/widget.mixin.js
+++ public/javascripts/controllers/widget.mixin.js
@@ -16,6 +16,7 @@ define([
 
       var didToCall = (typeof did === 'undefined') ? $scope.did : did;
       voxButtonElement.dataset.did = didToCall;
+      voxButtonElement.dataset.ringback = $scope.widget.ringback;
 
       var launch_call_button = document.getElementById('launch_call');
       if (launch_call_button)
